08f0b2afb747284f840c71d4772cd240ea6f0e18
Noah Portes Chaikin <noah@porteschaikin.com>
2014-01-09 18:17:52 -0500

    removed comments from seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 # user
 User.destroy_all
 first_user = User.create!(email: "noah@porteschaikin.com", username: "nporteschaikin", password: "test000", password_confirmation: "test000")
