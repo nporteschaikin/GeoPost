273ea7c3d2e800ce2aef97142f9b236310824da2
Noah Portes Chaikin <noah@porteschaikin.com>
2014-01-10 15:14:34 -0500

    interface fixes and such

--- app/models/assets/avatar.rb
+++ app/models/assets/avatar.rb
@@ -1,8 +1,7 @@
 module Assets
   class Avatar < Asset
     
-    has_attached_file   :attachment,
-                        styles: { small: "50x50#", medium: "75x75#", large: "150x150#" }
+    has_attached_file   :attachment, styles: { tiny: "35x35#", small: "50x50#", medium: "75x75#", large: "150x150#" }
     
   end
 end
