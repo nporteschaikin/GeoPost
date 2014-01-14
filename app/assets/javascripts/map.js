69fd60ef6962b981183b09b17ed12328dd8b6b75
Noah Portes Chaikin <noah@porteschaikin.com>
2014-01-14 17:27:52 -0500

    almost done with map tool

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,8 +1,10 @@
-function M (selector, bounds) {
+function M (selector, bounds, icon) {
   this.el         =   $(selector);
   this.bounds     =   bounds;
   this.setId();
   this.setMap();
+  if (icon) this.setIcon(icon);
+  return this;
 }
 
 M.prototype.setId = function () {
@@ -23,10 +25,32 @@ M.prototype.setMap = function () {
   // create tile layer
   var url   =   'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attr  =   'Map data © OpenStreetMap contributors',
-      layer =   new L.TileLayer (url, { minZoom: 8, maxZoom: 12, attribution: attr } );
+      layer =   new L.TileLayer (url, {attribution: attr });
+      
+  // set bounds
+  this.object
+    .fitBounds([
+      [this.bounds[0], this.bounds[1]],
+      [this.bounds[2], this.bounds[3]]
+    ]);
   
   // add tile layer to object
   this.object
     .addLayer(layer);
   
+}
+
+M.prototype.setIcon = function (url) {
+  this.icon   =   L.icon({iconUrl: url});
+  return this.icon;
+}
+
+M.prototype.addPlace = function(latlng) {
+  // currently just a namespace
+  this.addMarker(latlng);
+}
+
+M.prototype.addMarker = function (latlng) {
+  if (!this.icon) return false;
+  L.marker(latlng, {icon: this.icon}).addTo(this.object);
 }
