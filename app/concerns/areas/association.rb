7b86feafae1ebf1431685ca897bce788da122100
Noah Portes Chaikin <noah@porteschaikin.com>
2014-03-10 18:06:40 -0400

    hmmm....

--- app/concerns/areas/association.rb
+++ app/concerns/areas/association.rb
@@ -1,19 +1,21 @@
 module Areas
   module Association
-    
+
     extend ActiveSupport::Concern
-    
+
     module ClassMethods
-      
+
       def area_associations *associations
         associations.each do |association|
+
           define_method association do
             "#{association}".classify.constantize.within bounds
           end
+          
         end
       end
-      
+
     end
-    
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Places::Application.routes.draw do
-  
+
   root  to: "home#guest"
-  
+
   get   "/home"   => "home#user",   as: :user_home
-  
+
   get     "/in"   => "sessions#new",     as: :sign_in
   post    "/in"   => "sessions#create",  as: :new_session
   delete  "/out"  => "sessions#destroy", as: :sign_out
-  
-  resources   :users, :posts, :areas
-  
+
+  resources   :users, :posts, :areas, :places
+
 end
