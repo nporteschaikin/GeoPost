1972c2ecbe7d91401cc53f557b5285fbf7757fd5
Noah Portes Chaikin <noah@porteschaikin.com>
2014-03-11 09:10:14 -0400

    fixes

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,7 +1,8 @@
 class AreasController < ApplicationController
-  
+
   def show
-    @area   =   Area.find params[:id]
+    @area  = Area.find params[:id]
+    @post  = Post.new unless current_user?(@area.user)
   end
-  
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < ApplicationController
-  
+
   before_filter   :signed_in,    only: :user
   before_filter   :signed_out,   only: :guest
-  
+
   def guest
   end
-  
+
   def user
-    
+    @post  = Post.new
   end
-  
+
 end
